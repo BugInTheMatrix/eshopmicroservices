0dcd323011a51d2af5d6ac594764695d80ed6986
rsaw007 <124884240+BugInTheMatrix@users.noreply.github.com>
2025-12-22 14:13:09 +0530

    [Rahul] validator added

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -8,10 +8,23 @@ namespace Catalog.API.Products.CreateProduct
     public record CreateProductCommand(string Name,List<string> Categories,string Description,string ImageFile,decimal Price)
         :ICommand<CreateProductResult>;
     public record CreateProductResult(Guid Id);
+    public class CreateProductCommandValidater : AbstractValidator<CreateProductCommand>
+    {
+        public CreateProductCommandValidater()
+        {
+            RuleFor(x => x.Name).NotEmpty().MaximumLength(100).WithMessage("Name is required");
+            RuleFor(x => x.Categories).NotEmpty().WithMessage("category is required");
+            RuleFor(x => x.Description).NotEmpty().MaximumLength(500).WithMessage("descri[tion is required"); ;
+            RuleFor(x => x.ImageFile).NotEmpty().MaximumLength(200).WithMessage("imagefile is required"); ;
+            RuleFor(x => x.Price).GreaterThan(0).WithMessage("price is required"); ;
+        }
+    }
     public class CreateProductHandler(IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
         public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
+            
+
             Product product = new Product()
             {
                 Name=command.Name,
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,14 +1,18 @@
-﻿using Carter;
+﻿using BuildingBlocks.Behaviors;
+using Carter;
 using MediatR;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddCarter();
 
 builder.Services.AddMediatR(config =>
 {
     config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
 });
+builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly);
+builder.Services.AddCarter();
+
 builder.Services.AddMarten(options =>
 {
     options.Connection(builder.Configuration.GetConnectionString("Database")!);
