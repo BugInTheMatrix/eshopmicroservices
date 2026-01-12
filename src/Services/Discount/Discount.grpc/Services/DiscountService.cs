2b23f35b846d6efa5656d37e3956ae75de275c09
rsaw007 <124884240+BugInTheMatrix@users.noreply.github.com>
2026-01-12 15:10:33 +0530

    [Rahul] Added Getdiscount route

---
+++ src/Services/Discount/Discount.grpc/Services/DiscountService.cs
@@ -0,0 +1,36 @@
+﻿using Discount.grpc.Data;
+using Discount.grpc.Models;
+using Grpc.Core;
+using Mapster;
+using Microsoft.EntityFrameworkCore;
+
+namespace Discount.grpc.Services
+{
+    public class DiscountService
+        (DiscountContext dbContext,ILogger<DiscountService> logger): DiscountProtoService.DiscountProtoServiceBase
+    {
+        public override async Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+        {
+            var coupon = await dbContext.Coupons.FirstOrDefaultAsync(x=>x.ProductName==request.ProductName);
+            if (coupon is null)
+                coupon = new Coupon { ProductName = "No Discount", Amount = 0, Description = "No Discount Desc" };
+
+            logger.LogInformation("Discount is retrieved for ProductName : {productName}, Amount : {amount}", coupon.ProductName, coupon.Amount);
+
+            var couponModel = coupon.Adapt<CouponModel>();
+            return couponModel;
+        }
+        public override Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
+        {
+            return base.CreateDiscount(request, context);
+        }
+        public override Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
+        {
+            return base.UpdateDiscount(request, context);
+        }
+        public override Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context)
+        {
+            return base.DeleteDiscount(request, context);
+        }
+    }
+}
