405bcb19663f439c62c25a459f23b82765ccaa14
rsaw007 <124884240+BugInTheMatrix@users.noreply.github.com>
2026-04-15 12:47:41 +0530

    [rahul]

---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/DeleteOrder/DeleteOrderCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Orders.Commands.DeleteOrder
+{
+    public record DeleteOrderCommand(OrderDto Order) : ICommand<DeleteOrderResult>;
+    public record DeleteOrderResult(bool IsDeleted);
+    public class DeleteOrderValidator : AbstractValidator<DeleteOrderCommand> 
+    {
+        public DeleteOrderValidator()
+        {
+                RuleFor(x => x.Order.Id).NotEmpty().WithMessage("Order Id is required");
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/DeleteOrder/DeleteOrderhandler.cs
@@ -0,0 +1,26 @@
+﻿using Ordering.Application.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Orders.Commands.DeleteOrder
+{
+    public class DeleteOrderhandler(IApplicationDbContext dbContext) : ICommandHandler<DeleteOrderCommand, DeleteOrderResult>
+    {
+        public async Task<DeleteOrderResult> Handle(DeleteOrderCommand command, CancellationToken cancellationToken)
+        {
+            var orderId = OrderId.Of(command.Order.Id);
+            var order = await dbContext.Orders.FindAsync([orderId], cancellationToken);
+            if (order is null)
+            {
+                throw new OrderNotFoundException(command.Order.Id);
+            }
+            dbContext.Orders.Remove(order);
+            await dbContext.SaveChangesAsync(cancellationToken);
+            return new DeleteOrderResult(true);
+
+        }
+    }
+}
